e51deaafadc01fd2014e6610310e507af9ba5de3
Matthias <matthiasdanzmayr@crayTitan.local>
2013-01-31 17:37:37 -0500

    Initial Commit

---
+++ IRDishControl/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  IRDishControl
+//
+//  Created by Matthias on 1/31/13.
+//  Copyright (c) 2013 Matthias. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
